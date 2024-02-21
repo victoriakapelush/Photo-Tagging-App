dfe65404b79c07f550d8b19d8a565b9d00eac924
victoriakapelush <victoriakapelush@gmail.com>
2024-02-20 21:21:01 -0500

    Add a popup window

--- server/app.js
+++ server/app.js
@@ -36,7 +36,6 @@ app.use(express.static('uploads'));
 
 // Define routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
