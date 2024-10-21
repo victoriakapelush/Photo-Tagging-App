a955ba3035ad84debc21dd11f2b491258b11e30f
victoriakapelush <victoriakapelush@gmail.com>
2024-10-21 00:11:39 -0400

    Fix typo in GameOverPopup component

--- client/src/components/GameOverPopup.jsx
+++ client/src/components/GameOverPopup.jsx
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/GameRulesPopup.css";
+import "../styles/gameRulesPopup.css";
 
 function GameOverPopup({ gameOverMessage, game }) {
   const topPercentage = game === "game-one" ? "5%" : "10%";
